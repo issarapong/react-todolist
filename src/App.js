17902cb2a00a77eeb7df7f6cec8b6d78ebd62cf1
IS <mr.issarapong@gmail.com>
2023-04-24 17:01:33 +0700

    finish:typo

--- src/App.js
+++ src/App.js
@@ -1,28 +1,73 @@
-import './App.scss';
-import { FaHome } from 'react-icons/fa';
+import "./App.scss";
+import {
+  FaHome,
+  FaInbox,
+  FaCalendar,
+  FaCalendarAlt,
+  FaChevronDown,
+} from "react-icons/fa";
 
 function App() {
   return (
-    <div className='container'>
-     {/* Header */}
-     <header className='header'>
-     <span>
-  <FaHome />
-</span>
-      <h3>TODOLIST</h3>
+    <div className="container">
+      {/*Header*/}
+      <header className="header">
+        <span>
+          <FaHome />
+        </span>
+        <h3>CC-Todolist</h3>
       </header>
 
+      {/*Sidebar*/}
+      <aside className="sidebar">
+        <section className="sidebar_generic">
+          <ul className="generic_lists">
+            <li className="active">
+              <span>
+                <FaInbox />
+              </span>
+              <h6>Inbox</h6>
+            </li>
+            <li>
+              <span>
+                <FaCalendar />
+              </span>
+              <h6>Today</h6>
+            </li>
+            <li>
+              <span>
+                <FaCalendarAlt />
+              </span>
+              <h6>Next 7 Days</h6>
+            </li>
+          </ul>
+        </section>
+        <section className="sidebar_middle">
+          <span>
+            <FaChevronDown />
+          </span>
+          <h6>Projects</h6>
+        </section>
+        <section className="sidebar_projects">
+          <ul className="project_lists">
+            <li className="active">
+              <span>
+                <FaInbox />
+              </span>
+              <h6>Project-A</h6>
+            </li>
+            <li>
+              <span>
+                <FaCalendar />
+              </span>
+              <h6>Project-B</h6>
+            </li>
+          </ul>
+        </section>
+      </aside>
 
-
-     {/* Sidebar*/}
-     <aside className='sidebar'>Sidebar</aside>
-
-
-
-
-     {/* content */}
-     <main className='content'>Content</main>
-
+      {/*content*/}
+      <main className="content">content</main>
     </div>
   );
 }
