9b79a07b98b064e1e072c318791457263e3ef8cf
IS <mr.issarapong@gmail.com>
2023-04-25 13:26:22 +0700

    finish:internal state ui form

--- src/components/Todo/TodoForm.jsx
+++ src/components/Todo/TodoForm.jsx
@@ -1,16 +1,57 @@
 import styles from './TodoForm.module.scss';
+import React, { useState } from 'react';
 
-export function TodoForm() {
+export function TodoForm({onSetIsAddMode}) {
+    // # 1 : Logic Section
+    const [task, setTask] = useState('');
+    const [isError,setIsError] = useState(false)
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+        // validate
+        if(task.trim() === '') {
+            setIsError(true)
+            return;
+        }
+
+        // set back to normal mode
+		onSetIsAddMode(false)
+	
+    };
+    const handleClickCancel = (e) => {
+		onSetIsAddMode(false)
+	
+
+    };
+
+    const handleChangeInput = (e) => {
+        setIsError(false)
+		setTask(e.target.value)
+    };
+
+ 
+
+    // # 2 : UI-Section
     return (
-        <form className={styles.todo__form__container}>
-            <input className={styles.todo__form__input} placeholder='Task Name' />
+        <form className={styles.todo__form__container} onSubmit={handleSubmit}>
+            <input
+                className={styles.todo__form__input}
+                placeholder='Task Name'
+                value={task}
+                onChange={handleChangeInput}
+            />
             <div className={styles.todo__form__footer}>
-                <p className={styles.todo__error}>Title is required</p>
+               {isError &&  <p className={styles.todo__error}>Task Name is required</p>}
                 <div className={styles.todo__form__buttons}>
-                    <button>Cancel</button>
-                    <button>Add Task</button>
+                    <button type='button' onClick={handleClickCancel}>
+                        Cancel
+                    </button>
+                    <button type='submit'>Add Task</button>
                 </div>
             </div>
+
         </form>
     );
 }
+
